715ea5349e9e25559d3b7d3a3dad031f38285894
Egor0108 <ehor01012008@gmail.com>
2025-09-14 18:31:12 +0300

    co

---
+++ .vscode/qqw.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+int main() { 
+double touble, start, end, step; 
+std::cout << "Введите начальное значение x: "; 
+std::cin >> start; 
+std::cout << "Введите конечное значение x: "; 
+std::cin >> end;  
+std::cout << "Введите шаг: "; 
+std::cin >> step; 
+ 
+for (double x = start; x <= end; x += step) { 
+double y = sin(x); // функция y = sin(x)   
+std::cout << x << " | " << y << std::endl; 
+} 
+return 0; 
+}
